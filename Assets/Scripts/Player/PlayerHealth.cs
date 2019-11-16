5363ebb71bb4259a1b6fe6866dd173222983d5d8
Jan Behrens <jan.behrens@student.hpi.de>
2019-11-16 16:32:33 +0100

    Added player health management system and global update caller

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerHealth : MonoBehaviour
+{
+
+    //Object that stores and manages the health of a player
+
+    public int maxHealth;
+    public int currentHealth;
+    public bool isAlive;
+    public int timeLastDeath;
+    public int respawnTime;
+
+
+    public PlayerHealth(int maxhealth, int respawntime)
+    {
+        this.maxHealth = maxhealth;
+        this.currentHealth = this.maxHealth;
+        this.isAlive = true;
+        this.timeLastDeath = 0;
+        this.respawnTime = respawntime;
+
+        UpdateCaller.OnUpdate += Update;
+    }
+
+    ~PlayerHealth()
+    {
+        UpdateCaller.OnUpdate -= Update;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if player has died since last frame, add function with despawn, ... later
+        if(currentHealth <= 0 && isAlive)
+        {
+            isAlive = false;
+            timeLastDeath = Toolkit.currentTime();
+        }
+        //Revive player after enough time has elapsed, add function with respawn, ... later
+        if (!isAlive)
+        {
+            if((Toolkit.currentTime() - timeLastDeath) >= respawnTime)
+            {
+                currentHealth = maxHealth;
+                isAlive = true;
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Tools/UpdateCaller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateCaller : MonoBehaviour
+{
+    //Registers other objects so that they can use the update event
+    private static UpdateCaller instance = null;
+    public static System.Action OnUpdate;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else if (this != instance)
+            Destroy(this);
+    }
+
+    void Update()
+    {
+        if (OnUpdate != null)
+            OnUpdate();
+    }
+}
