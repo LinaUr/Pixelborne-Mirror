575e41a722fb786dac91e59d39e4a4cc2ac62e51
Jan Behrens <jan.behrens@student.hpi.de>
2019-11-23 11:18:51 +0100

    Added fade to black

--- Assets/Scripts/Tools/Toolkit.cs
+++ Assets/Scripts/Tools/Toolkit.cs
@@ -13,6 +13,16 @@ public static class Toolkit
         return ((int)(System.DateTime.UtcNow.Subtract(new System.DateTime(1970, 1, 1))).TotalSeconds);
     }
 
+    public static int currentTimeMilliseconds()
+    {
+        return ((int)(System.DateTime.UtcNow.Subtract(new System.DateTime(1970, 1, 1))).TotalMilliseconds);
+    }
+
+    public static int currentTimeMillisecondsToday()
+    {
+        return ((int)(System.DateTime.UtcNow.Subtract(System.DateTime.Today)).TotalMilliseconds);
+    }
+
 
 
 }
