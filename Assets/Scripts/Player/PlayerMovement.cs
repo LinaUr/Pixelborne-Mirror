215b88084d8c85fe2cc3bf113f5372973ff3532d
Alexander Junger <alexander.junger@student.hpi.uni-potsdam.de>
2019-11-28 14:42:48 +0100

    try to resolve unity errors

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Experimental.Input;
-using UnityEngine.Experimental.Input.Plugins.PlayerInput;
-
-public class PlayerMovement : MonoBehaviour
-{
-    public float moveSpeed = 10f;
-    public float jumpForce = 20f;
-    public bool facingRight = true;
-
-    public Transform groundCheckPoint;
-    public float groundCheckRadius = 0.2f;
-    public LayerMask whatIsGround;
-    public bool isGrounded = true;
-
-    private Rigidbody2D rigidbody2D;
-
-    void Start ()
-    {
-        rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
-    }
-
-    public void OnJump(InputValue value)
-    {
-        if (isGrounded)
-        {
-            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpForce);
-        }
-    }
-
-    public void OnMovement(InputValue value)
-    {
-        // CONTROLS
-        var moveX = value.Get<float>();
-
-        // ANIMATIONS
-
-        // PLAYER DIRECTION
-        if (moveX < 0.0f && facingRight)
-        {
-            FlipPlayer();
-        }
-        else if (moveX > 0.0f && !facingRight)
-        {
-            FlipPlayer();
-        }
-
-        // PHYSICS
-        var rigibody2D = gameObject.GetComponent<Rigidbody2D>();
-        rigibody2D.velocity = new Vector2(moveX * moveSpeed, rigibody2D.velocity.y);
-    }
-
-    private void FlipPlayer()
-    {
-        facingRight = !facingRight;
-        gameObject.transform.Rotate(new Vector3(0f, 180f, 0f), Space.Self);
-    }
-
-}
-=======
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -216,4 +148,3 @@ public class PlayerMovement : MonoBehaviour
     }
 
 }
->>>>>>> origin/4-make-camera-switch-between-level-screens
