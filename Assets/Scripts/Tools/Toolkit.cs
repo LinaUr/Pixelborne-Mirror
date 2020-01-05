18bb91797ac3e0f9cd86c2cc6c13077929bb1e5e
Jan Behrens <jan.behrens@student.hpi.de>
2020-01-05 18:36:06 +0100

    updated CameraMover and fixed some small consistence issues in Toolkit and UpdateCaller

--- Assets/Scripts/Tools/Toolkit.cs
+++ Assets/Scripts/Tools/Toolkit.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 // This Class contains various miscellaneous utility methods for other classes
 public static class Toolkit
 {
-    // This method returns the current time in the unix format
+    // This Method returns the current time in the unix format
     public static int currentTime()
     {
         return ((int)(System.DateTime.UtcNow.Subtract(new System.DateTime(1970, 1, 1))).TotalSeconds);
     }
 
-    // This method returns the number of milliseconds that passed since the start of the current day
+    // This Method returns the number of milliseconds that passed since the start of the current day
     public static int currentTimeMillisecondsToday()
     {
         return ((int)(System.DateTime.UtcNow.Subtract(System.DateTime.Today)).TotalMilliseconds);
