3c99eab582bdc9fea50f67b39323bec5f59fb73a
Alexander Junger <alexander.junger@student.hpi.uni-potsdam.de>
2020-01-26 14:37:40 +0100

    added pause menu and functionality to open it in-game plus rename of SampleScene in Singleplayer

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0;
+            SceneManager.LoadScene("PauseMenu", LoadSceneMode.Additive);
+        }
+
+    }
+}
