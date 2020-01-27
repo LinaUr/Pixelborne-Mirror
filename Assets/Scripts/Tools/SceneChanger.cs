d7f8174d1eaa726470f00d07b1d8796c6eff4ab3
Lina <dhr92447@cndps.com>
2020-01-27 19:57:30 +0100

    added PauseMenu to SceneChanger

--- Assets/Scripts/Tools/SceneChanger.cs
+++ Assets/Scripts/Tools/SceneChanger.cs
@@ -6,14 +6,18 @@ public class SceneChanger
     // The index must be taken from the build settings.
     static readonly int MAIN_MENU_SCENE_INDEX = 0;
 
+    // PAUSE_MENU_SCENE_INDEX represents the build index of the MainMenu scene.
+    // The index must be taken from the build settings.
+    static readonly int PAUSE_MENU_SCENE_INDEX = 1;
+
     // MULTIPLAYER_SCENE_INDEX represents the build index of the Multiplayer scene.
     // The index must be taken from the build settings.
-    static readonly int MULTIPLAYER_SCENE_INDEX = 1;
+    static readonly int MULTIPLAYER_SCENE_INDEX = 2;
 
     // TODO: uncomment when Singleplayer Scene gets introduced.
     // SINGLEPLAYER_SCENE_INDEX represents the build index of the Singleplayer scene.
     // The index must be taken from the build settings.
-    //static readonly int SINGLEPLAYER_SCENE_INDEX = 2;
+    //static readonly int SINGLEPLAYER_SCENE_INDEX = 3;
 
     // This method sets the Multiplayer scene as the active scene.
     public static void SetMultiplayerAsActiveScene()
@@ -25,6 +29,12 @@ public class SceneChanger
     public static void SetMainMenuAsActiveScene()
     {
         SceneManager.LoadScene(MAIN_MENU_SCENE_INDEX);
+    }
+
+    // This method sets the PauseMenu scene as the active scene.
+    public static void SetPauseMenuAsActiveScene()
+    {
+        SceneManager.LoadScene(PAUSE_MENU_SCENE_INDEX);
     }
 
     // TODO: uncomment when Singleplayer Scene gets introduced.
