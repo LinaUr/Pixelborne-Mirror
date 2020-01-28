0c284f89876b30ef2a8d92331ff0672114e234f5
Alexander Junger <alexander.junger@student.hpi.uni-potsdam.de>
2020-01-28 17:12:24 +0100

    corrected comments and reordered methods of SceneChanger for same order in MainMenu

--- Assets/Scripts/Tools/SceneChanger.cs
+++ Assets/Scripts/Tools/SceneChanger.cs
@@ -6,18 +6,18 @@ public class SceneChanger
     // The index must be taken from the build settings.
     static readonly int MAIN_MENU_SCENE_INDEX = 0;
 
-    // MULTIPLAYER_SCENE_INDEX represents the build index of the Multiplayer scene.
+    // SINGLEPLAYER_SCENE_INDEX represents the build index of the Singleplayer scene.
     // The index must be taken from the build settings.
     static readonly int SINGLEPLAYER_SCENE_INDEX = 1;
     
-    // SINGLEPLAYER_SCENE_INDEX represents the build index of the Singleplayer scene.
+    // MULTIPLAYER_SCENE_INDEX represents the build index of the Multiplayer scene.
     // The index must be taken from the build settings.
     static readonly int MULTIPLAYER_SCENE_INDEX = 2;
 
-    // This method sets the Multiplayer scene as the active scene.
-    public static void SetMultiplayerAsActiveScene()
+    // This method sets the MainMenu scene as the active scene.
+    public static void SetMainMenuAsActiveScene()
     {
-        SceneManager.LoadScene(MULTIPLAYER_SCENE_INDEX);
+        SceneManager.LoadScene(MAIN_MENU_SCENE_INDEX);
     }
     
     // This method sets the Singleplayer scene as the active scene.
@@ -26,9 +26,9 @@ public class SceneChanger
         SceneManager.LoadScene(SINGLEPLAYER_SCENE_INDEX);
     }
 
-    // This method sets the MainMenu scene as the active scene.
-    public static void SetMainMenuAsActiveScene()
+    // This method sets the Multiplayer scene as the active scene.
+    public static void SetMultiplayerAsActiveScene()
     {
-        SceneManager.LoadScene(MAIN_MENU_SCENE_INDEX);
+        SceneManager.LoadScene(MULTIPLAYER_SCENE_INDEX);
     }
 }
