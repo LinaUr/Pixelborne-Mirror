d2541147ee691e3711194b2e20052e791f26cac7
Lina <dhr92447@cndps.com>
2020-02-05 23:04:35 +0100

    Button navigation via Keyboard and Gamepad; added Backbutton to Credits

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    private Image sliderFillImage;
+
+    void Start()
+    {
+        // Lock and hide curser
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // A Slider in Unity can only set a highlight color when it is dragged or moved.
+    // Since we only use the keyboard and gamepad for menu navigation we want it highlighted when it is selected.
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject.GetComponent<Slider>() != null)
+        {
+            // Highlight the Slider of the menu when it is selected.
+            if (sliderFillImage == null)
+            {
+                sliderFillImage = EventSystem.current.currentSelectedGameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>();
+            }
+            if (sliderFillImage.color.r != 0.8f)
+            {
+                Color color = sliderFillImage.color;
+                color.r = 8f;
+                sliderFillImage.color = color;
+            }
+        }
+        else if (sliderFillImage.color.r != 0.6f)
+        {
+            // Unhighlight the Slider of the menu when it is not selected.
+            Color color = sliderFillImage.color;
+            color.r = 0.6f;
+            sliderFillImage.color = color;
+        }
+    }
+}
