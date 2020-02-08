70c476b964a1a18f42f2c4c1b073308dc6adb169
Lina <dhr92447@cndps.com>
2020-02-08 16:18:23 +0100

    fixed CreditsScroller and back button

--- Assets/Scripts/Menu/CreditsScroller.cs
+++ Assets/Scripts/Menu/CreditsScroller.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CreditsScroller : MonoBehaviour
 {
@@ -7,25 +8,34 @@ public class CreditsScroller : MonoBehaviour
     [SerializeField]
     private GameObject m_mainMenu;
 
-    Vector3 m_originalPos;
+    private Vector3 m_originalPos;
     
-    void Start()
+    void Awake()
     {
         // Save the original position of credits for reset.
-        m_originalPos = m_credits.transform.position;
+        m_originalPos = gameObject.transform.position;
     }
-    
+
+    private void OnEnable()
+    {
+        gameObject.transform.position = m_originalPos;
+    }
+
     void Update()
     {
         // Scroll the credits.
-        m_credits.transform.Translate(Vector3.up * 0.05f);
+        gameObject.transform.Translate(Vector3.up * 0.05f);
 
         // If the credits are out of screen, reset the position of credits and activate the main menu.
-        if (transform.position.y >= 15)
+        if (gameObject.transform.position.y >= 15)
         {
-            m_credits.SetActive(false);
-            m_credits.transform.position = m_originalPos;
             m_mainMenu.SetActive(true);
+            m_credits.SetActive(false);
         }
+    }
+
+    private void OnDisable()
+    {
+        EventSystem.current.SetSelectedGameObject(EventSystem.current.firstSelectedGameObject);
     }
 }
