210322ff036ef3f1ee39d44001858a0957302858
Lina <dhr92447@cndps.com>
2020-02-08 13:13:18 +0100

    fixed comment, added constants for color

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -4,11 +4,14 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    private Image sliderFillImage;
+    private Image m_sliderFillImage;
+
+    private const float m_highlight = 0.8f;
+    private const float m_unhighlight = 0.6f;
 
     void Start()
     {
-        // Lock and hide curser
+        // Lock and hide curser.
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -16,26 +19,29 @@ public class Menu : MonoBehaviour
     // Since we only use the keyboard and gamepad for menu navigation we want it highlighted when it is selected.
     private void Update()
     {
+        // Check if a Slider is selected.
         if (EventSystem.current.currentSelectedGameObject.GetComponent<Slider>() != null)
         {
+            // Not all Menus have a Slider, but when it does we grab it.
+            if (m_sliderFillImage == null)
+            {
+                m_sliderFillImage = EventSystem.current.currentSelectedGameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>();
+            }
             // Highlight the Slider of the menu when it is selected.
-            if (sliderFillImage == null)
+            if (m_sliderFillImage.color.r != m_highlight)
             {
-                sliderFillImage = EventSystem.current.currentSelectedGameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>();
+                Color color = m_sliderFillImage.color;
+                color.r = m_highlight;
+                m_sliderFillImage.color = color;
             }
-            if (sliderFillImage.color.r != 0.8f)
-            {
-                Color color = sliderFillImage.color;
-                color.r = 8f;
-                sliderFillImage.color = color;
-            }
-            else if (sliderFillImage.color.r != 0.6f)
-            {
-                // Unhighlight the Slider of the menu when it is not selected.
-                Color color = sliderFillImage.color;
-                color.r = 0.6f;
-                sliderFillImage.color = color;
-            }
+            
+        }
+        else if (m_sliderFillImage != null && m_sliderFillImage.color.r != m_unhighlight)
+        {
+            // Unhighlight the Slider of the menu when it is not selected.
+            Color color = m_sliderFillImage.color;
+            color.r = m_unhighlight;
+            m_sliderFillImage.color = color;
         }
     }
 }
