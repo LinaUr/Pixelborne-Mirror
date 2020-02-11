d1b382a342ac76970221a08fa23a971f4c32046c
Lina <dhr92447@cndps.com>
2020-02-11 22:32:35 +0100

    added payment and logging to selling screen

--- Assets/Scripts/Menu/SellingScreen.cs
+++ Assets/Scripts/Menu/SellingScreen.cs
@@ -1,44 +1,78 @@
 ﻿using TMPro;
 using UnityEngine;
 using System.IO;
+using System.Globalization;
 
 public class SellingScreen : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI m_fileTextMesh;
+    [SerializeField]
+    private TextMeshProUGUI m_priceTextMesh;
+
+    private string m_fileToSell = string.Empty;
+    private string m_priceToPay = string.Empty;
+
+    private const float m_DEFAULT_PRICE = 1.00f;
 
     void Start()
     {
-        // Freeze game.
-        Time.timeScale = 0;
-        GameMediator.Instance.LockPlayerInput(true);
+        GameMediator.Instance.FreezeGame();
 
         // Set camera of canvas.
         Canvas canvas = gameObject.GetComponent<Canvas>();
         canvas.worldCamera = Camera.main;
 
         // Set file for sell on canvas.
-        m_fileTextMesh.SetText(Path.GetRandomFileName());
+        m_fileToSell = Path.GetRandomFileName();
+        m_fileTextMesh.SetText(m_fileToSell);
+
+        if (GameMediator.Instance.PriceToPay < m_DEFAULT_PRICE)
+        {
+            GameMediator.Instance.PriceToPay = m_DEFAULT_PRICE;
+        }
+
+        // Set price to pay in format of US currency on canvas.
+        m_priceToPay = GameMediator.Instance.PriceToPay.ToString("C2", CultureInfo.CreateSpecificCulture("en-US"));
+        m_priceTextMesh.SetText(m_priceToPay);
     }
 
-    // This method resumes the gameplay.
-    public void FileSellApproved()
+    // This method resumes the gameplay and logs the sold file.
+    public void SellFile()
     {
-        // Unfreeze game.
-        Time.timeScale = 1;
-        SceneChanger.UnloadSellingScreenAdditive();
-        GameMediator.Instance.LockPlayerInput(false);
+        Log($"Sold {m_fileToSell}");
 
-        // TODO: Log sold file.
+        UnfreezeGame();
     }
 
-    public void FileSellRejected()
+    // This method resumes the gameplay and logs the payed price.
+    public void PayPrice()
     {
-        // Unfreeze game.
-        Time.timeScale = 1;
-        SceneChanger.UnloadSellingScreenAdditive();
-        GameMediator.Instance.LockPlayerInput(false);
+        
+        Log($"Payed {m_priceToPay}");
+        GameMediator.Instance.PriceToPay *= 1.25f;
+
+        UnfreezeGame();
+    }
+
+    public void RejectAll()
+    {
+        UnfreezeGame();
 
         // TODO: Reset game to checkpoint and respawn all enemies.
+    }
+
+    private void UnfreezeGame()
+    {
+        SceneChanger.UnloadSellingScreenAdditive();
+        GameMediator.Instance.UnfreezeGame();
+    }
+
+    private void Log(string logMessage)
+    {
+        using (StreamWriter w = File.AppendText("SellingLog.txt"))
+        {
+            Toolkit.Log(logMessage, w);
+        }
     }
 }
