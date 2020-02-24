c767ab56ee6e120c368a179b3d1bf059a855935b
Lina <dhr92447@cndps.com>
2020-02-24 19:49:28 +0100

    applied review

--- Assets/Scripts/Camera/CameraMultiplayer.cs
+++ Assets/Scripts/Camera/CameraMultiplayer.cs
@@ -56,6 +56,7 @@ public class CameraMultiplayer : MonoBehaviour, ICamera
         if (Time.timeScale > 0)
         {
             // Time is not frozen.
+
             if (m_stopwatchPauseFade.IsRunning)
             {
                 m_stopwatchPauseFade.Stop();
@@ -68,6 +69,7 @@ public class CameraMultiplayer : MonoBehaviour, ICamera
         else
         {
             // Time is frozen.
+
             if (!m_stopwatchPauseFade.IsRunning)
             {
                 // Start measuring the time of the freeze.
@@ -91,16 +93,16 @@ public class CameraMultiplayer : MonoBehaviour, ICamera
         Color color = m_fadeImage.GetComponent<SpriteRenderer>().color;
 
         // Complete the fade when enough time has passed.
-        bool doCompleteFade = elapsedTime - m_fadeStartTime >= m_fadeTime;
+        bool isFadeComplete = elapsedTime - m_fadeStartTime >= m_fadeTime;
 
         if (m_fadeMode == FadeMode.FadeOut)
         {
             // Lower the opacity of the fade to black canvas object based on the time passed since the fade to black trigger.
             float percentage = takenTime / m_fadeTime;
-            color.a = doCompleteFade ? 1.0f : percentage;
+            color.a = isFadeComplete ? 1.0f : percentage;
             m_fadeImage.GetComponent<SpriteRenderer>().color = color;
 
-            if (doCompleteFade)
+            if (isFadeComplete)
             {
                 FadeCompleted();
                 GameMediator.Instance.FadedOut();
@@ -108,12 +110,12 @@ public class CameraMultiplayer : MonoBehaviour, ICamera
         }
         else if (m_fadeMode == FadeMode.FadeIn)
         {
-            // Increase the opacity of the fade to black canvas object based on the time passed since the fade in trigger. 
+            // Increase the opacity of the fade to black canvas object based on the time passed since fade was triggered. 
             float percentage = 1 - takenTime / m_fadeTime;
-            color.a = doCompleteFade ? 0.0f : percentage;
+            color.a = isFadeComplete ? 0.0f : percentage;
             m_fadeImage.GetComponent<SpriteRenderer>().color = color;
 
-            if (doCompleteFade)
+            if (isFadeComplete)
             {
                 FadeCompleted();
                 GameMediator.Instance.FadedIn();
@@ -125,7 +127,7 @@ public class CameraMultiplayer : MonoBehaviour, ICamera
         }
     }
 
-    // This methods resets important variables after a fade finished.
+    // This method resets important variables after a fade finished.
     private void FadeCompleted()
     {
         m_fadeMode = FadeMode.NoFade;
