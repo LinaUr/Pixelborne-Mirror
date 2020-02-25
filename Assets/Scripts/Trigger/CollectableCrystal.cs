ddb3daf8774c36266bb4364f9509ba8039adcb44
Lina <dhr92447@cndps.com>
2020-02-25 18:27:29 +0100

    made crystal collectable, added pling sound effect

---
+++ Assets/Scripts/Trigger/CollectableCrystal.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class CollectableCrystal : MonoBehaviour
+{
+    private AudioSource m_audioPlayer;
+    private bool m_isCollected;
+
+    private void Start()
+    {
+        m_audioPlayer = gameObject.GetComponent<AudioSource>();
+        m_isCollected = false;
+    }
+
+    void Update()
+    {
+        if (m_isCollected && !m_audioPlayer.isPlaying)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (!m_isCollected)
+        {
+            //if (collider == Singleplayer.Instance.Player)
+            if (GameMediator.Instance.ActivePlayers.Contains(collider.gameObject))
+            {
+                m_isCollected = true;
+                m_audioPlayer.Play();
+            }
+        }
+    }
+}
