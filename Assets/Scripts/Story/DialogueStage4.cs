5a8632a63e7538ca4ac3a6163fff4972c757ef64
Jana Trenti <jana.trenti@student.hpi.de>
2020-04-04 22:02:07 +0200

    dialogue not triggering

--- Assets/Scripts/Story/DialogueStage4.cs
+++ Assets/Scripts/Story/DialogueStage4.cs
@@ -116,6 +116,8 @@ public class DialogueStage4 : MonoBehaviour
         PlayerProgressed = false;
         m_enemiesKilled = false;
         m_dialogueMode = DialogueMode.NotStarted;
+        DisableCollisionsOfPrincessWithOtherEntities();
+        Singleplayer.Instance.ActiveEnemies.Remove(m_princess);
         m_princess.GetComponent<EnemyAttackAndMovement>().StartFollowPlayer();
     }
 
@@ -177,7 +179,14 @@ public class DialogueStage4 : MonoBehaviour
                 break;
         }
     }
-    
+
+    private void DisableCollisionsOfPrincessWithOtherEntities()
+    {
+        int playerLayer = LayerMask.NameToLayer("Player");
+        int enemyLayer = LayerMask.NameToLayer("Enemy");
+        Physics2D.IgnoreLayerCollision(playerLayer, m_princess.layer, true);
+        Physics2D.IgnoreLayerCollision(enemyLayer, m_princess.layer, true);
+    }
 
     public bool EnemiesKilled()
     {
