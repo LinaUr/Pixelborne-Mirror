2ddf9bfb77b7753cece7b8515813e6e86110c7e8
Martin Buessemeyer <Martin.Buessemeyer@student.hpi.de>
2020-04-05 20:36:30 +0200

    THE MORE THE BETTER IT IS BETTER

--- Assets/Scripts/Camera/SimpleColorFilter.cs
+++ Assets/Scripts/Camera/SimpleColorFilter.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using UnityEngine;
 
-// This class implements a simple color filter that gets stronger the further you go in a stage.
-/// <summary></summary>
+/// <summary>This class implements a simple color filter that gets stronger the further you go in a stage.</summary>
 public class SimpleColorFilter : MonoBehaviour
 {
     [SerializeField]
@@ -16,6 +15,7 @@ public class SimpleColorFilter : MonoBehaviour
     [SerializeField]
     private GameObject m_filterImage;
     
+    /// <summary>Recalculates the opacity and sets it on the images.</summary>
     void Update()
     {
         float opacity;
