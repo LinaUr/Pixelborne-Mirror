6767cbc5b4ff691cf8fb6525e009b327d3a4c91f
Alexander Junger <alexander.junger@student.hpi.uni-potsdam.de>
2020-04-05 22:35:04 +0200

    adapted Entity.cs

--- Assets/Scripts/Entity/Entity.cs
+++ Assets/Scripts/Entity/Entity.cs
@@ -1,56 +1,56 @@
 ﻿using UnityEngine;
 using UnityEngine.Experimental.Input.Plugins.PlayerInput;
 
-/// <summary>This class is the base class for all entities that can execute actions like walking or attacking.
-/// This class unifies the duplicate state and behavior from <see cref="EnemyActions"/> and <see cref="PlayerActions"/>.
+/// <summary>Is the base class for all entities that can execute actions like walking or attacking.
+/// It unifies the duplicate state and behaviour from <see cref="EnemyActions"/> and <see cref="PlayerActions"/>.
 /// </summary>
 public abstract class Entity : MonoBehaviour, IAttack
 {
-    /// <summary>The weapon collider from the entity</summary>
+    /// <summary>The weapon collider from the entity.</summary>
     [SerializeField]
     protected BoxCollider2D m_weaponCollider;
-    /// <summary>Is the entity facing in right</summary>
+    /// <summary>Is the entity facing in right.</summary>
     [SerializeField]
     protected bool m_isFacingRight;
     [SerializeField]
     private float m_distanceInWhichEntityCountsAsGrounded = 0.1f;
-    /// <summary>The jump force of the entity</summary>
+    /// <summary>The jump force of the entity.</summary>
     [SerializeField]
     protected float m_jumpForce = 22.0f;
-    /// <summary>The move speed of the entity</summary>
+    /// <summary>The move speed of the entity.</summary>
     [SerializeField]
     protected float m_moveSpeed = 10.0f;
-    /// <summary>The attack damage of the entity</summary>
+    /// <summary>The attack damage of the entity.</summary>
     [SerializeField]
     protected int m_attackDamage = 1;
     [SerializeField]
     private LayerMask m_whatIsGround;
 
-    /// <summary>The animator of the entity</summary>
+    /// <summary>The animator of the entity.</summary>
     protected Animator m_animator;
-    /// <summary>The rigidbody of the entity</summary>
+    /// <summary>The rigidbody of the entity.</summary>
     protected Rigidbody2D m_rigidbody2D;
-    /// <summary>The collider of the entity</summary>
+    /// <summary>The collider of the entity.</summary>
     protected BoxCollider2D m_collider;
     /// <summary>The entity health of the entity. Can be null in order to create an invincible entity.</summary>
     protected EntityHealth m_entityHealth;
 
-    /// <summary>Is the entity grounded</summary>
+    /// <summary>Is the entity grounded.</summary>
     protected bool m_isGrounded = false;
-    /// <summary>The current attacking direction</summary>
+    /// <summary>The current attacking direction.</summary>
     protected int m_currentAttackingDirection = 0;
     /// <summary>The horizontal is grounded distance. Is used to smooth jumping.</summary>
     protected static readonly float HORIZONTAL_IS_GROUNDED_DISTANCE = 0.1f;
     /// <summary>The vertical is grounded distance. Is used to smooth jumping.</summary>
     protected static readonly float VERTICAL_IS_GROUNDED_DISTANCE = 0.2f;
-    /// <summary>The attack animator parameter names</summary>
+    /// <summary>The attack animator parameter names.</summary>
     protected static readonly string[] ATTACK_ANIMATOR_PARAMETER_NAMES = { "AttackingUp", "Attacking", "AttackingDown" };
-    /// <summary>The jumping animator parameter name</summary>
+    /// <summary>The jumping animator parameter name.</summary>
     protected static readonly string JUMPING_ANIMATOR_PARAMETER_NAME = "IsJumping";
-    /// <summary>The speed animator parameter name</summary>
+    /// <summary>The speed animator parameter name.</summary>
     protected static readonly string SPEED_ANIMATOR_PARAMETER_NAME = "Speed";
 
-    /// <summary>The death zones name</summary>
+    /// <summary>The death zones name.</summary>
     public static readonly string DEATH_ZONES_NAME = "DeathZones";
     /// <summary>Gets or sets a value indicating whether this instance is input locked.</summary>
     /// <value>
@@ -65,7 +65,7 @@ public abstract class Entity : MonoBehaviour, IAttack
     ///   <c>true</c> if this instance is rolling; otherwise, <c>false</c>.</value>
     public bool IsRolling { get; protected set; } = false;
 
-    /// <summary>Awakes this instance by setting all referenced components</summary>
+    /// <summary>Awakes this instance by setting all referenced components.</summary>
     protected virtual void Awake()
     {
         m_animator = gameObject.GetComponent<Animator>();
@@ -111,13 +111,13 @@ public abstract class Entity : MonoBehaviour, IAttack
         gameObject.transform.localScale = currentScale;
     }
 
-    /// <summary>This method executes the common behavior when an entity is dying.</summary>
+    /// <summary>Executes the common behavior when an entity is dying.</summary>
     protected virtual void Die()
     {
         StopAttacking();
     }
 
-    /// <summary>Called when [trigger enter2 d].</summary>
+    /// <summary>Called when entered the trigger.</summary>
     /// <param name="collider">The collider that entered the entity collider.</param>
     protected virtual void OnTriggerEnter2D(Collider2D collider)
     {
@@ -146,8 +146,8 @@ public abstract class Entity : MonoBehaviour, IAttack
         }
     }
 
-    /// <summary>Called when teh entity should jump. e.g. the player presses the jump button.</summary>
-    /// <param name="value">The Inputvalue which is not used but necessary to fit the PlayerInput-Interface from Unity.</param>
+    /// <summary>Called when the entity should jump e.g. the player presses the jump button.</summary>
+    /// <param name="value">The input value which is not used but necessary to fit the PlayerInput-Interface from Unity.</param>
     protected void OnJump(InputValue value)
     {
         if (!IsInputLocked && !IsRolling && m_isGrounded)
@@ -182,16 +182,16 @@ public abstract class Entity : MonoBehaviour, IAttack
     }
 
  
-    /// <summary> StartAttacking and StopAttacking are triggered by the attack animations
-    /// in order to mark the time window where the attack deals damage.</summary>
+    /// <summary> StartAttacking is triggered by the attack animations
+    ///     in order to mark the time window where the attack deals damage.</summary>
     public void StartAttacking()
     {
         m_weaponCollider.enabled = true;
         m_weaponCollider.isTrigger = true;
     }
 
-    /// <summary> StartAttacking and StopAttacking are triggered by the attack animations
-    /// in order to mark the time window where the attack deals damage.</summary>
+    /// <summary> StopAttacking is triggered by the attack animations
+    ///     in order to mark the time window where the attack deals damage.</summary>
     public virtual void StopAttacking()
     {
         m_weaponCollider.enabled = false;
@@ -208,19 +208,19 @@ public abstract class Entity : MonoBehaviour, IAttack
 
     /// <summary>Determines whether the entity [is facing right].</summary>
     /// <returns>
-    ///   <c>true</c> if [is facing right]; otherwise, <c>false</c>.</returns>
+    ///     <c>true</c> if [is facing right]; otherwise, <c>false</c>.</returns>
     public bool IsFacingRight()
     {
         return m_isFacingRight;
     }
 
     /// <summary>Determines whether the attack in cancelling.
-    /// Attacks cancel each other if they are on the same height, both are currently in the deal damage window
-    /// and the facing direction is not the same.</summary>
+    ///     Attacks cancel each other if they are on the same height, both are currently in the deal damage window
+    ///     and the facing direction is not the same.</summary>
     /// <param name="attackDirectionFromOtherEntity">The attack direction from the other entity.</param>
-    /// <param name="otherEntityIsFacingRight">if the other entity is facing right</param>
+    /// <param name="otherEntityIsFacingRight">If the other entity is facing right.</param>
     /// <returns>
-    ///   <c>true</c> if the attack is canceled; otherwise, <c>false</c>.</returns>
+    ///     <c>true</c> if the attack is canceled; otherwise, <c>false</c>.</returns>
     public bool IsAttackCancelling(int attackDirectionFromOtherEntity, bool otherEntityIsFacingRight)
     {
         return (attackDirectionFromOtherEntity == m_currentAttackingDirection) && m_weaponCollider.enabled
